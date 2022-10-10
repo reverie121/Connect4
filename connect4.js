0ab497608dcb3501db92fae9fda3508eccae55da
Jeremy Sobczak <code@jerspace.com>
2022-10-10 14:36:05 -0400

    Minor code cleanup.

--- connect4.js
+++ connect4.js
@@ -16,6 +16,7 @@ const gameBoard = document.getElementById('game');
 const htmlBoardTop = document.getElementById('board-top');
 const htmlBoard = document.getElementById('board');
 const htmlBoardFeet = document.getElementById('board-feet');
+const endGamePopUp = document.getElementById('endgame-button');
 
 // Board Size initialization
 let WIDTH;
@@ -24,61 +25,65 @@ let turnCounter;
 
 // Active Player Initialization
 let currPlayer = 1;
-let pColor1 = 'rgb(170, 170, 0)';
-let pColor2 = 'rgb(170, 170, 0)';
+let pColor1;
+let pColor2;
+let currColor;
 setTimeout(() => {
-    pColor1 = 'red';
-    pColor2 = 'blue';
-    let currColor = pColor1;
-    colorTitle();
+    setTitleColors();
 }, 600)
 
+// Opacity initialization for fade()
+form.style.opacity = 1;
+htmlBoardTop.style.opacity = 1;
+
 // Color characters in game title using alternating player colors
-const colorTitle = () => {
+function colorTitle() {
     setTimeout(() => {
-        gameTitle.style.opacity = '1';
+        gameTitle.style.opacity = 1;
     }, 200);
     const chars = gameTitle.children;
     for (let i = 0; i < chars.length; i++) {
         if (Math.floor(i % 2) == 0) {
-            chars[i].style.color = `${pColor1}`;
+            chars[i].style.color = pColor1;
         } else {
-            chars[i].style.color = `${pColor2}`;
+            chars[i].style.color = pColor2;
         }
     }
 };
 
-// Change game title colors when player uses select dropdowns
-p1ColSel.onchange = () => {
-    pColor1 = `${p1ColSel.value}`;
-    switchP();
-    switchP();
+// Set color variables to dropdown selections
+function setPlayerColors() {
+    pColor1 = p1ColSel.value;
+    pColor2 = p2ColSel.value;
+    currColor = pColor1;
+}
+
+// Set color variables and game title characters to dropdown selections
+function setTitleColors() {
+    setPlayerColors();
     colorTitle();
 }
-p2ColSel.onchange = () => {
-    pColor2 = `${p2ColSel.value}`;
-    switchP();
-    switchP();
-    colorTitle();
-}
+
+// Change game title colors when color selection dropdowns are used
+p1ColSel.onchange = () => setTitleColors();
+p2ColSel.onchange = () => setTitleColors();
+
+// Toggle opacity between 0 and 1
+const fade = element => element.style.opacity == 0 ? element.style.opacity = 1 : element.style.opacity = 0
 
 // Logic for new game form submission and game launch
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    const heightSel = document.getElementById('boardHeight');
-    const widthSel = document.getElementById('boardWidth');
     if (p1ColSel.value == p2ColSel.value) {
         alert('Please select two different player colors.')
     } else {
-        HEIGHT = heightSel.value;
-        WIDTH = widthSel.value;
+        HEIGHT = document.getElementById('boardHeight').value;
+        WIDTH = document.getElementById('boardWidth').value;
         turnCounter = HEIGHT * WIDTH;
-        pColor1 = `${p1ColSel.value}`;
-        pColor2 = `${p2ColSel.value}`;
-        currColor = pColor1;
+        setPlayerColors();
         makeBoard(board);
-        gameTitle.style.opacity = '0';
-        form.style.opacity = '0';
+        fade(gameTitle)
+        fade(form);
         setTimeout(() => {
             form.style.display = 'none';
             gameBoard.style.display = 'block';
@@ -91,10 +96,10 @@ form.addEventListener("submit", function(e) {
 })
 
 // Create a backend JS board (array) to check for win conditions
-const makeBoard = arr => {
+function makeBoard(arr) {
     // Erase any existing array contents
-    while (board.length > 0) {
-        board.pop();
+    while (arr.length > 0) {
+        arr.pop();
     }
     // Create new board
     for (let i = 0; i < HEIGHT; i++) {
@@ -106,27 +111,41 @@ const makeBoard = arr => {
     }
 };
 
-// Make the UI board
-const makeHtmlBoard = () => {
-    // Removes any previous board contents
-    clearBoard(htmlBoardTop);
-    clearBoard(htmlBoard);
-    // Adds a table row on top for the players to drop their pieces in
-    // and triggers the handleClick function when clicked.
-    var top = document.createElement("tr");
-    top.addEventListener("click", handleClick);
-    top.addEventListener("mouseover", onHover);
-    // Removes player color from cell when mouse moves off.
-    top.addEventListener("mouseout", e => e.target.style.backgroundColor = '');
-    // Adds (WIDTH) cells to the board top and gives them sequential numeric IDs.
+// Make target change to player color when hovered over
+function onHover(actionTarget) {
+    actionTarget.addEventListener("mouseover", e => e.target.style.backgroundColor = currColor);
+    actionTarget.addEventListener("mouseout", e => e.target.style.backgroundColor = '');
+};
+
+// Call handleClick when target is clicked
+function onClick(actionTarget) {
+    actionTarget.addEventListener("click", handleClick);
+};
+
+// Adjust board feet spacing for board width
+function adjustFeetSpacing() {
+    const feet = document.getElementById('board-feet').children;
+    feet[0].style.marginRight = '30%';
+    feet[1].style.marginLeft = '30%';
+}
+
+// Generate top row for dropping pieces into the board
+function makeTop() {
+    const top = document.createElement("tr");
+    onHover(top);
+    onClick(top); // Add (WIDTH) cells to the board top and give them sequential numeric IDs
     for (let x = 0; x < WIDTH; x++) {
         const headCell = document.createElement("td");
         headCell.setAttribute("id", x);
         top.append(headCell);
     }
     htmlBoardTop.append(top);
-    // Adds (HEIGHT) rows to the board with (WIDTH) cells/columns each.
-    // Gives each cell an ID corresonding to their location ('row-col').
+}
+
+// Generate main board where pieces will be held
+function makeMainBoard() {
+    // Add (HEIGHT) rows to the board with (WIDTH) cells/columns each
+    // and give each cell an ID corresonding to its location ('row-col')
     for (let y = 0; y < HEIGHT; y++) {
         const row = document.createElement("tr");
         for (let x = 0; x < WIDTH; x++) {
@@ -136,49 +155,27 @@ const makeHtmlBoard = () => {
         }
         htmlBoard.append(row);
     }
+}
 
-    // Add feet to board - disabled due to margin problem but saved for posterity
-    // for (i = 0; i < 2; i++) {
-    //     let aFoot = document.createElement('div');
-    //     aFoot.className = 'foot';
-    //     let footTop = document.createElement('div');
-    //     footTop.className = 'top-foot';
-    //     let footBottom = document.createElement('div');
-    //     footBottom.className = 'bottom-foot';
-    //     let footLeft = document.createElement('div');
-    //     footLeft.className = 'left-foot';
-    //     let footRight = document.createElement('div');
-    //     footRight.className = 'right-foot';
-    //     // footLeft.style.marginRight = `${WIDTH / -50}rem`;
-    //     // footRight.style.marginLeft = `${WIDTH / -50}rem`;
-    //     footBottom.append(footLeft);
-    //     footBottom.append(footRight);
-    //     aFoot.append(footTop);
-    //     aFoot.append(footBottom);
-    //     htmlBoardFeet.append(aFoot);
-    // }
-    // while (htmlBoardFeet.children.length > 2) {
-    //     htmlBoardFeet.firstChild.remove();
-    // }
 
-    // Adjust board feet spacing
-    const feet = document.getElementById('board-feet').children;
-    feet[0].style.marginRight = '30%';
-    feet[1].style.marginLeft = '30%';
+// Make the UI board
+function makeHtmlBoard() {
+    // Remove any previous board contents
+    clearBoard(htmlBoardTop);
+    clearBoard(htmlBoard);
+    // Add a table row on top for the players to drop their pieces in
+    // and triggers the handleClick function when clicked.
+    makeTop();
+    makeMainBoard();
+    adjustFeetSpacing();
+    // Fade in game board
     setTimeout(() => {
-        htmlBoardTop.style.opacity = '1';
-        htmlBoard.style.opacity = '1';
-        htmlBoardFeet.style.opacity = '1';
+        fade(gameBoard);
     }, 500);
-}; // end of makeHtmlBoard
-
-// Change top row spot to player's color when hovered over
-const onHover = e => {
-    e.target.style.backgroundColor = currColor;
 };
 
 // Locate the lowest empty space in column x
-const findSpotForCol = x => {
+function findSpotForCol(x) {
     for (let i = 0; i < HEIGHT; i++) {
         if (board[i][x] != null) {
             if (i == 0) {
@@ -191,7 +188,7 @@ const findSpotForCol = x => {
 }
 
 // Insert a div into the correct table cell to create a player piece
-const placeInTable = (y, x) => {
+function placeInTable(y, x) {
     const spot = document.getElementById(`${y}-${x}`);
     spot.innerHTML = `<div></div>`;
     spot.firstChild.style.backgroundColor = `${currColor}`;
@@ -199,7 +196,7 @@ const placeInTable = (y, x) => {
 };
 
 // Change the active player
-const switchP = () => {
+function changePlayer(dropSpot) {
     if (currPlayer == 1) {
         currPlayer = 2;
         currColor = pColor2;
@@ -207,30 +204,25 @@ const switchP = () => {
         currPlayer = 1;
         currColor = pColor1;
     }
+    dropSpot.target.style.backgroundColor = currColor; // Change hover color to next player's color
 };
 
 // Reset variables in preperation for a new game and adjusts visibiity of elements
-const resetGame = () => {
-    const newGameButton = document.getElementById('endgame-button');
-    setTimeout(() => {
-        newGameButton.style.display = "none";
+function resetGame() {
+    setTimeout(() => { // <---make method
         document.getElementById('endgame-overlay').style.display = "none";
         gameBoard.style.display = 'none';
         form.style.display = 'block';
     }, 300);
-    setTimeout(() => {
-        newGameButton.style.opacity = '1';
-        form.style.opacity = '1';
-        pColor1 = `${p1ColSel.value}`;
-        pColor2 = `${p2ColSel.value}`;
-        currColor = pColor1;
-        colorTitle();
+    setTimeout(() => { // <---make method
+        endGamePopUp.style.display = "none";
+        fade(htmlBoardTop)
+        fade(form);
+        setTitleColors();
     }, 1000);
-    newGameButton.style.opacity = '0';
-    htmlBoardTop.style.opacity = '0';
-    htmlBoard.style.opacity = '0';
-    htmlBoardFeet.style.opacity = '0';
-    gameTitle.style.opacity = '0';
+    fade(endGamePopUp)
+    fade(gameBoard);
+    fade(gameTitle);
     document.getElementById('endgame-button').removeEventListener('click', resetGame);
     currPlayer = 1;
     pColor1 = 'rgb(170, 170, 0)';
@@ -242,10 +234,11 @@ const resetGame = () => {
 };
 
 // Respond to player click with gameplay features
-const handleClick = evt => {
-    // Get column number from ID of clicked cell
-    const x = +evt.target.id;
-    // Get lowest empty spot in column x (if none, ignore click)
+function handleClick(e) {
+
+    // Get column number from ID of clicked cell and convert string number to int
+    const x = +e.target.id;
+    // Get lowest empty spot in specified column (if full, ignore click)
     const y = findSpotForCol(x);
     if (y === null) {
         return;
@@ -253,28 +246,29 @@ const handleClick = evt => {
     // Update JS board and place piece in HTML board
     board[y][x] = currPlayer;
     placeInTable(y, x);
+
     // Check for win
     if (checkForWin()) {
-        evt.target.style.backgroundColor = ''; // Remove current player color from last hovered cell
-        return endGame(`Player ${currPlayer} won in ${Math.round(((WIDTH*HEIGHT) - turnCounter + 1)/2)} turns!`);
+        e.target.style.backgroundColor = ''; // Remove current player color from last hovered cell
+        return endGame(`Player ${currPlayer} won in ${Math.round(((WIDTH*HEIGHT) - turnCounter + 1)/2)} turns!`); // <---make method rather than inline***
     }
+
     // Check for tie game
     else if (turnCounter == 1) {
         currColor = '#666';
-        evt.target.style.backgroundColor = ''; // Remove current player color from last hovered cell
+        e.target.style.backgroundColor = ''; // Remove current player color from last hovered cell
         return endGame('You both lose!');
     } else {
         turnCounter--;
     }
-    // Change active player
-    switchP();
-    onHover(evt);
+
+    changePlayer(e);
 }
 
 // Check for active player victory
-const checkForWin = evt => {
-    const _win = cells => {
-        // Check JS array and return all of the active player's controlled cells
+function checkForWin() {
+    function _win(cells) {
+        // Check array and return all of the active player's controlled cells
         return cells.every(
             ([y, x]) =>
             y >= 0 &&
@@ -287,32 +281,32 @@ const checkForWin = evt => {
     // Look for 4 consecutive cells within return of current player's cells
     for (let y = 0; y < HEIGHT; y++) {
         for (let x = 0; x < WIDTH; x++) {
-            let horiz = [ // Checks for a horizontal win
+            let horiz = [ // Condition for a horizontal win
                 [y, x],
                 [y, x + 1],
                 [y, x + 2],
                 [y, x + 3]
             ];
-            let vert = [ // Checks for a vertical win
+            let vert = [ // Condition for a vertical win
                 [y, x],
                 [y + 1, x],
                 [y + 2, x],
                 [y + 3, x]
             ];
-            let diagDR = [ // Checks for a diagnoal win, downward to the right
+            let diagDR = [ // Condition for a diagnoal win, downward to the right
                 [y, x],
                 [y + 1, x + 1],
                 [y + 2, x + 2],
                 [y + 3, x + 3]
             ];
-            let diagDL = [ // Checks for a diagonal win, downward to the left
+            let diagDL = [ // Condition for a diagonal win, downward to the left
                 [y, x],
                 [y + 1, x - 1],
                 [y + 2, x - 2],
                 [y + 3, x - 3]
             ];
-            // Uses _win return to check current player's cells for the above conditions.
-            // If any win conditions are true, returns true.
+            // Use _win return to search current player's cells for the above conditions
+            // and if any win conditions are true, return true
             if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
                 for (let i = 0; i < 4; i++) {
                     // Function for marking winning spots
@@ -322,7 +316,7 @@ const checkForWin = evt => {
                         spot.style.border = `solid 2rem ${currColor}`;
                         spot.firstChild.style.backgroundColor = 'beige';
                     };
-                    // Marks the pieces from the first (top-left) instance of each win type
+                    // Mark the pieces according to the first (top-left) instance of each win type
                     if (_win(horiz)) {
                         const winSpot = document.getElementById(`${y}-${x+i}`);
                         markWinners(winSpot);
@@ -350,51 +344,45 @@ const checkForWin = evt => {
 // popUp adapted from http://www.developphp.com/video/JavaScript/Custom-Alert-Box-Programming-Tutorial
 function popUp() {
     this.render = (msg) => {
-        let winW = window.innerWidth;
-        let winH = window.innerHeight;
-        const dropSpots = document.getElementById('board-top').firstChild.children;
+        const winW = window.innerWidth;
+        const winH = window.innerHeight;
         const letters = gameTitle.children;
-        const dialogBox = document.getElementById('endgame-button');
-        const dialogOverlay = document.getElementById('endgame-overlay');
+        const overlay = document.getElementById('endgame-overlay');
         // Manifests an overlay that prevents further play
-        dialogOverlay.style.display = "block";
-        dialogOverlay.style.height = `${winH}px`;
+        overlay.style.display = "block";
+        overlay.style.height = `${winH}px`;
         // Manifests a button with victory message to reset game
-        dialogBox.style.left = `${(winW/2) - (200)}px`;
-        dialogBox.style.top = '6rem';
-        dialogBox.style.display = 'block';
-        dialogBox.innerText = `${msg}`;
+        endGamePopUp.style.left = `${(winW/2) - (200)}px`;
+        endGamePopUp.style.top = '6rem';
+        endGamePopUp.style.display = 'block';
+        endGamePopUp.innerText = `${msg}`;
         // Makes top/drop row invisible
-        for (i = 0; i < dropSpots.length; i++) {
-            dropSpots[i].style.opacity = '0';
-        }
+        fade(htmlBoardTop);
+        setTimeout(() => (fade(endGamePopUp)), 100);
         // Sets title letters to original body background color
         for (i = 0; i < letters.length; i++) {
             letters[i].style.color = 'beige';
         }
         // Sets the page background and message text to winning player's color
-        dialogBox.style.color = currColor;
+        endGamePopUp.style.color = currColor;
         body.style.backgroundColor = currColor;
         // Allows for game reset
         setTimeout(() => {
-            dialogBox.innerText = 'Click For New Game';
-            dialogBox.addEventListener('click', resetGame);
+            endGamePopUp.innerText = 'Click For New Game';
+            endGamePopUp.addEventListener('click', resetGame);
         }, 2000);
     }
 }
 
 // Announce game result
-const endGame = msg => {
+function endGame(msg) {
     let endGameMsg = new popUp();
     setTimeout(() => endGameMsg.render(msg), 200);
 };
 
 // Function for clearing the board for game reset
-const clearBoard = (toClear) => {
+function clearBoard(toClear) {
     while (toClear.firstChild) {
         toClear.removeChild(toClear.lastChild);
     }
 };
-
-// Initialize game title
-colorTitle();
